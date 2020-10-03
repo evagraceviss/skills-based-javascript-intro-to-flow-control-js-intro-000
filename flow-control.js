381f9155ff2717503e20cb332e9fb69e49c0a112
Eva Viss <evagraceviss@gmail.com>
2020-10-03 02:48:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,8 +4,10 @@ function basicTeenager(age) {
   }
 }
 
-function teenager() {
-
+function teenager(age) {
+  if (age >= 13 && age <=19) {
+    return "You are a teenager!"
+  }
 }
 
 function ageChecker(age) {
